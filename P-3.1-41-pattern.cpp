f00ac491e39ee73555d39b6af2cc71951bc65d92
Muskan yadav <muskanyadav92163@gmail.com>
2025-07-01 22:10:32 +0530

    P-3.1-Circlet

--- P-3.1-41-pattern.cpp
+++ P-3.1-41-pattern.cpp
@@ -1,1 +1,19 @@
+// 41 
+// 41 42 
+// 41 42 43 
+// 41 42 43 44 
+// 41 42 43 44 45
 
+#include<iostream>
+using namespace std;
+int main(){
+    
+    for(int i=1;i<=5;i++){
+        for(int j=1;j<=i;j++){
+            cout << "4" << j << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}

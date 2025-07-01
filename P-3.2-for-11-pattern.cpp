7ec1d5294723488cec3af86e94c8790dad437100
Muskan yadav <muskanyadav92163@gmail.com>
2025-07-01 22:12:53 +0530

    P-3.2-Circlet

--- P-3.2-for-11-pattern.cpp
+++ P-3.2-for-11-pattern.cpp
@@ -1,1 +1,20 @@
+// 11
+// 12 13 
+// 14 15 16 
+// 17 18 19 20
 
+#include<iostream>
+using namespace std;
+int main(){
+    int m=11;
+
+    for(int i=1;i<=4;i++){
+        for(int j=1;j<=i;j++){
+            cout << m << " ";
+            m++;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}

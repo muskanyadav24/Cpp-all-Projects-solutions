547ee167b5e21503ce4b438e3e2859d553b13ee6
Muskan yadav <muskanyadav92163@gmail.com>
2025-07-01 22:15:58 +0530

    P.3-3-Circlet

--- P.3-3-space-5.cpp
+++ P.3-3-space-5.cpp
@@ -1,1 +1,22 @@
+//     5
+//    45
+//   345
+//  2345
+// 12345
 
+#include<iostream>
+using namespace std;
+int main(){
+
+    for(int i=5;i>=1;i--){
+        for(int k=1;k<i;k++){
+            cout << " ";
+        }
+        for(int j=i;j<=5;j++){
+            cout << j;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}

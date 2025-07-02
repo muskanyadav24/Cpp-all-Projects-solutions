d93e68abe2bd9498d43dd13fa44b186cd5b4c567
Muskan yadav <muskanyadav92163@gmail.com>
2025-07-02 08:24:11 +0530

    P-3.-5-Circlet

--- P-3.-5-pyramid.cpp
+++ P-3.-5-pyramid.cpp
@@ -1,1 +1,25 @@
+//     5
+//    454
+//   34543
+//  2345432
+// 123454321
 
+#include<iostream>
+using namespace std;
+int main(){
+
+    for(int i=5;i>=1;i--){
+        for(int k=1;k<i;k++){
+            cout << " ";
+        }
+        for(int j=i;j<=5;j++){
+            cout << j;
+        }
+        for(int j=4;j>=i;j--){
+        cout << j;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}

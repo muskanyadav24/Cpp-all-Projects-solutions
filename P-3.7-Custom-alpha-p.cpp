dd8029ed394d278453a12a6b14edf4387fe713ac
Muskan yadav <muskanyadav92163@gmail.com>
2025-07-02 08:48:41 +0530

    P-3.7-Circlet

--- P-3.7-Custom-alpha-p.cpp
+++ P-3.7-Custom-alpha-p.cpp
@@ -1,1 +1,27 @@
+// * * * * * 
+// *       * 
+// * * * * * 
+// *        
+// *       
 
+#include<iostream>
+using namespace std;
+int main(){
+
+    for(int i=1;i<=5;i++){
+        for(int j=1;j<=5;j++){
+            if(i==1 || i==3 ||j==1 ){
+                cout << "*" << " ";
+            }else if(i==2 && j==5){
+                cout << "*" << " ";
+            }else if(j==2 || j==3 || j==4){
+                cout << "  ";
+            }else if(i==4 && j==5){
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}

1506de15c63f35c6cdfb91d007bf76615aa0de6a
Muskan yadav <muskanyadav92163@gmail.com>
2025-07-02 08:22:03 +0530

    P-3.4-Circlet

--- P-3.4-for-1-0.cpp
+++ P-3.4-for-1-0.cpp
@@ -1,1 +1,26 @@
+// 1 0 1 0 1
+//   1 0 1 0
+//     1 0 1
+//       1 0
+//         1
 
+#include<iostream>
+using namespace std;
+int main(){
+
+    for(int i=1;i<=5;i++){
+        for(int k=1;k<i;k++){
+            cout << " ";
+        }
+        for(int j=5;j>=i;j--){
+            if(j%2==0){
+                cout << "0";
+            }else{
+                cout << "1";
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}

d0e284f656ab1fe7c2985e222ba89f3fcc7fe3bc
Muskan yadav <muskanyadav92163@gmail.com>
2025-07-03 20:57:47 +0530

    P-4.1-complete

--- P-4.1-largest-ele.cpp
+++ P-4.1-largest-ele.cpp
@@ -1,1 +1,35 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	int rowcol,largest=0;
+	int array[100][100];
 
+    cout <<"Enter the array row and column size :- ";
+    cin >> rowcol;
+
+	cout << "Enter array element :- " << endl;
+	
+	for(int i=0;i<rowcol;i++){
+		for(int j=0;j<rowcol;j++){
+			cout << "Array [" << i << "]["<< j << "] = ";
+			cin >> array[i][j];
+		}	
+	}
+	for (int i=0;i<rowcol;i++){
+        for(int j=0;j<rowcol;j++){
+           cout<< array[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+	for(int i=0;i<rowcol;i++){
+		for(int j=0;j<rowcol;j++){
+			if(largest < array[i][j]){
+				largest = array[i][j];
+			}
+		}
+	}
+	cout << "The largest element is :- " << largest << endl;
+	
+	return 0;
+}

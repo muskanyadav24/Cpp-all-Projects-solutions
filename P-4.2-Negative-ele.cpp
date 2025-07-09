640a7e1626be2aaaf8b5318b2bee247efda3b274
Muskan yadav <muskanyadav92163@gmail.com>
2025-07-09 21:06:31 +0530

    m

--- P-4.2-Negative-ele.cpp
+++ P-4.2-Negative-ele.cpp
@@ -1,1 +1,27 @@
-
+#include<iostream>
+using namespace std;
+int main(){
+	
+	int size;
+	
+	cout << "Enter the array size :- ";
+	cin >> size;
+	
+	int array[100];
+	
+	for(int i=0;i<size;i++){
+		cout << "Array [" << i << "]" << " = ";
+			cin >> array[i]; 
+	}
+		
+	cout << endl << "Negative element from an array :- ";
+		
+	for(int i=0;i<size;i++){
+		if(array[i]<0){
+			cout << array[i] << " ," ;
+		}else{
+			cout << " ";
+		}
+	}
+	return 0;
+}

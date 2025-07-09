0e5720e53552e651b086d8b8266f133e1bd9ca48
Muskan yadav <muskanyadav92163@gmail.com>
2025-07-09 21:17:51 +0530

    P-4.3-Assortment

--- P-4.3-Transpose-matrix.cpp
+++ P-4.3-Transpose-matrix.cpp
@@ -1,1 +1,30 @@
+#include<iostream>
+using namespace std;
+int main(){
 
+    int size,m=0;
+	int array[100][100];
+
+    cout <<"Enter the array row and column size :- ";
+    cin >> size;
+
+	cout << "Enter array element :- " << endl;
+	
+	for(int i=0;i<size;i++){
+		for(int j=0;j<size;j++){
+			cout << "Array [" << i << "]["<< j << "] = ";
+			cin >> array[i][j];
+		}	
+	}
+
+    cout << endl << "The transpose matrix of an array : " << endl;
+
+    for(int i=0;i<size;i++){
+		for(int j=0;j<size;j++){
+			cout << array[j][i] << " ";
+		}	
+        cout << endl;
+	}
+
+    return 0;
+}

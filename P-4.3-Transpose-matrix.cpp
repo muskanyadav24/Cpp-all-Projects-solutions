e51c2e2da0d784a7b8f1a7ed827093b274f6c7ea
Muskan yadav <muskanyadav92163@gmail.com>
2025-07-09 21:22:03 +0530

    P-4.3-Assortment

--- P-4.3-Transpose-matrix.cpp
+++ P-4.3-Transpose-matrix.cpp
@@ -28,3 +28,22 @@ int main(){
 
     return 0;
 }
+
+// OUTPUT :-
+
+// Enter the array row and column size :- 3
+// Enter array element :- 
+// Array [0][0] = 1
+// Array [0][1] = 2
+// Array [0][2] = 3
+// Array [1][0] = 4
+// Array [1][1] = 5
+// Array [1][2] = 6
+// Array [2][0] = 7
+// Array [2][1] = 8
+// Array [2][2] = 9
+
+// The transpose matrix of an array : 
+// 1 4 7 
+// 2 5 8 
+// 3 6 9 

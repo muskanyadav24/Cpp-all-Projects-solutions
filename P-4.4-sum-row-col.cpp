f62d2d6eecbfc0e23ae2884566bc320e8cc29655
Muskan yadav <muskanyadav92163@gmail.com>
2025-07-11 20:52:17 +0530

    P-4.4-Assortment

--- P-4.4-sum-row-col.cpp
+++ P-4.4-sum-row-col.cpp
@@ -1,1 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
 
+    int size,sum=0,row,col;
+	int array[100][100];
+
+    cout <<"Enter the array row and column size :- ";
+    cin >> size;
+
+	cout << "Enter array element :- " << endl;
+	
+	for(int i=0;i<size;i++){
+		for(int j=0;j<size;j++){
+			cout << "Array [" << i << "]["<< j << "] = ";
+			cin >> array[i][j];
+		}	
+	}
+    cout << "Enter the row Number :- " ;
+    cin >> row;
+    cout << "Enter the column Number :- " ;
+    cin >> col;
+
+    return 0;
+}
